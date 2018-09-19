83e330d2df8f615428ae672e5c5dd629bcde5add
nWhitcome <whitcome@iastate.edu>
2018-09-19 15:13:40 -0500

    Added to my experiments folder

--- experiments/nathan_experiments/index.js
+++ experiments/nathan_experiments/index.js
@@ -4,9 +4,11 @@ var server = require('http').Server(app);
 var port = 3000;
 var leaflet = require('leaflet');
 
+console.log("Server running");
+
 server.listen(port, "127.0.0.1", function () {
 });
 
 app.get('/', function(request, response){
-	response.sendFile(__dirname + '/index.html')
+	response.sendFile(__dirname + '/index.html');
 });
