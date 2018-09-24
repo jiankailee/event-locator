5fbcdc00fabfc956af606b45204684609bf699eb
nWhitcome <whitcome@iastate.edu>
2018-09-24 15:22:46 -0500

    updated react tests

--- experiments/nathan_experiments/React_Tests/my-app/src/App.js
+++ experiments/nathan_experiments/React_Tests/my-app/src/App.js
@@ -11,17 +11,22 @@ class App extends Component {
   render() {
     const position = [this.state.lat, this.state.lng]
     return (
-      <Map className="map" center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React Map Test</h1>
+        </header>
+        <Map className="map" center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker>
-      </Map>
+          </Marker>
+        </Map>
+      </div>
     );
   }
 }
