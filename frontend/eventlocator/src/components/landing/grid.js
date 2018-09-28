b719fb4272c2f48a819190f722634f1ba6b63c01
Jian Kai <jiankai@iastate.edu>
2018-09-28 14:46:42 -0500

    align map

--- frontend/eventlocator/src/components/landing/grid.js
+++ frontend/eventlocator/src/components/landing/grid.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import List from './list'
+import List from './list';
+import Mapbox from './mapbox';
 
 const styles = theme => ({
   root: {
@@ -30,6 +31,12 @@ function CenteredGrid(props) {
           <List/>
           <Paper className={classes.paper}>xs=3</Paper>
         </Grid>
+        
+        <Grid item xs={9}>
+         <Mapbox/>
+        
+      </Grid>
+       
       </Grid>
     </div>
   );
