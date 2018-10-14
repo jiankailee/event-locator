3141095b28272d9812749f593d4b118675904610
Jian Kai <jiankai@iastate.edu>
2018-10-14 11:49:52 -0500

    userpage

---
+++ frontend/eventlocator/src/components/user/userInfo.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+import AppBar from './AppBar';
+
+
+
+console.log(window.location.href);
+
+
+class userInfo extends Component {
+  render() {
+    return (
+      
+      <div display= 'flex'>
+      <AppBar/>
+      {/* <Grid/> */}
+      
+      </div>
+      
+    );
+  }
+}
+
+export default userInfo;
