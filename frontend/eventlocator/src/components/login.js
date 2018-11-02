27a3b902f12bebca01cf66eee7614fd01522cb75
Joel Wacker <jwacker@iastate.edu>
2018-11-01 20:48:05 -0500

    Editing hostnames to server names

--- frontend/eventlocator/src/components/login.js
+++ frontend/eventlocator/src/components/login.js
@@ -56,7 +56,7 @@ class login extends Component {
     this.getUsersInfo();
   }
   getUsersInfo=_=>{
-    fetch('http://localhost:8080/usersInfo') //http://proj309-tg-07.misc.iastate.edu:8080/usersInfo
+    fetch('http://proj309-tg-07.misc.iastate.edu:8080/usersInfo')
     .then(response=>response.json())
     .then(response=>this.setState({alluser: response.data}))
     // .then({data})=>{
