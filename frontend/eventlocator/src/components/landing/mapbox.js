f0bfac763bcef977fa92ce50ba368f21771674c4
tongdi <tongdi@iastate.edu>
2018-11-06 11:20:17 -0600

    add search-by-address

--- frontend/eventlocator/src/components/landing/mapbox.js
+++ frontend/eventlocator/src/components/landing/mapbox.js
@@ -4,20 +4,20 @@ import 'leaflet/dist/leaflet.css';
 import '../../App.css';
 import Map_search_bar from './map_search';
 import L from 'leaflet';
-import LocateControl from 'react-leaflet-locate-control'
+import LocateControl from 'react-leaflet-locate-control';
+import Geocode from 'react-geocode'
 
 delete L.Icon.Default.prototype._getIconUrl;
-
+Geocode.setApiKey("AIzaSyBgd1QZKo1cbcKgiHoWbr6pcBTsrssKzT4");
 const Example = ({components}) => (
     <div>
-        {components.map((component, i) => <Marker key={i} position={[component.latitude, component.longitude]}>
-        <Popup>
-            {component.eventName}
-        </Popup>
+        {components.map((component, i) => <Marker key={i} position={component[1]}>
+            <Popup>
+                {component[0]}
+            </Popup>
         </Marker>)}
     </div>
 )
-
 const locateOptions = {
     position: 'topright',
     strings: {
@@ -25,13 +25,25 @@ const locateOptions = {
     },
     onActivate: () => {} // callback before engine starts retrieving locations
   }
-
 L.Icon.Default.mergeOptions({
     iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
     iconUrl: require('leaflet/dist/images/marker-icon.png'),
     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
-
+let latLng = [];
+let test_info = [['Parks Library', [42.0281, -93.6488]], ['Memorial Union', [42.0237, -93.6459]]];
+Geocode.fromAddress("Physics Hall").then(
+    response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        latLng = [lat, lng]
+        //console.log(latLng);
+        let test1 = ['Add Event Here!', latLng];
+        test_info.push(test1);
+    },
+    error => {
+        console.error(error);
+    }
+)
 
 class Mapbox extends Component {
     state = {
@@ -54,10 +66,15 @@ class Mapbox extends Component {
         //console.log(this.state.alluser);
       }
 
-    render() {
-        console.log(this.state.allLocation)
-        // var test_info = [['Parks Library', [42.0281, -93.6488]], ['Memorial Union', [42.0237, -93.6459]]];
 
+
+
+    render() {
+
+
+        console.log(this.state.allLocation)
+        console.log(test_info)
+        // var test_info = [['Parks Library', [42.0281, -93.6488]], ['Memorial Union', [42.0237, -93.6459]]];
         // var test1 = ['Coover Hall', [42.0284, -93.6509]];
         // test_info.push(test1);
 
@@ -69,11 +86,10 @@ class Mapbox extends Component {
                         attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                         url= "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Example components={this.state.allLocation}/>
+                    <Example components={test_info}/>
                 </Map>
             </div >
         );
-
     }
 }
 
